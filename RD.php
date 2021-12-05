a8dc7bda01b7fd4ae0f03389f0cc9bce3062fa38
Mehmet Emrah TUNÇEL <timemrah@gmail.com>
2021-12-06 00:47:02 +0300

    Update RD.php

--- RD.php
+++ RD.php
@@ -7,41 +7,91 @@ class RD //REQUEST DATA
     private static $inputValues = null;
 
 
-
     public static function GET(string $key = null){
-        if($key === null){ return $_GET; }
-        return self::dotNestedKeySearchFromArray($key, $_GET);
+        if($_SERVER['REQUEST_METHOD'] !== 'GET'){ return []; }
+
+        if($_GET){
+            $values = $_GET;
+        } else{
+            if(self::$inputValues === null){ self::$inputValues = self::phpInputToValues(); }
+            $values = self::$inputValues;
+        }
+
+        if($key === null){ return $values; }
+        return self::dotNestedKeySearchFromArray($key, $values);
+
     }
     public static function positiveGET(string $key = null, $default = null){
+        if($_SERVER['REQUEST_METHOD'] !== 'GET'){ return []; }
+
         $value = self::GET($key);
         if(!is_numeric($value) || $value < 1){ return $default; }
-        return $value;
+        return (int) $value;
     }
 
 
     public static function POST(string $key = null){
-        if($key === null){ return $_POST; }
-        return self::dotNestedKeySearchFromArray($key, $_POST);
+        if($_SERVER['REQUEST_METHOD'] !== 'POST'){ return []; }
+
+        if($key === null && $_POST){ return $_POST; }
+        $value = self::dotNestedKeySearchFromArray($key, $_POST);
+        if($value){ return $value; }
+
+        //ALL VALUE TYPE
+        if(self::$inputValues === null){ self::$inputValues = self::phpInputToValues(); }
+        if($key === null){ return self::$inputValues; }
+        return self::dotNestedKeySearchFromArray($key, self::$inputValues);
+
     }
     public static function positivePOST(string $key = null, $default = null){
+        if($_SERVER['REQUEST_METHOD'] !== 'POST'){ return []; }
+
         $value = self::POST($key);
         if(!is_numeric($value) || $value < 1){ return $default; }
-        return $value;
+        return (int) $value;
     }
 
 
     public static function PUT(string $key = null){
+        if($_SERVER['REQUEST_METHOD'] !== 'PUT'){ return []; }
+
         if(self::$inputValues === null){ self::$inputValues = self::phpInputToValues(); }
         if($key === null){ return self::$inputValues; }
         return self::dotNestedKeySearchFromArray($key, self::$inputValues);
     }
     public static function positivePUT(string $key = null, $default = null){
+        if($_SERVER['REQUEST_METHOD'] !== 'PUT'){ return []; }
+
         $value = self::PUT($key);
         if(!is_numeric($value) || $value < 1){ return $default; }
-        return $value;
+        return (int) $value;
     }
-    
-    
+
+
+    public static function DELETE(string $key = null){
+        if($_SERVER['REQUEST_METHOD'] !== 'DELETE'){ return []; }
+
+        if($_GET){
+            $values = $_GET;
+        } else{
+            if(self::$inputValues === null){ self::$inputValues = self::phpInputToValues(); }
+            $values = self::$inputValues;
+        }
+
+        if($key === null){ return $values; }
+        return self::dotNestedKeySearchFromArray($key, $values);
+    }
+    public static function positiveDELETE(string $key = null, $default = null){
+        if($_SERVER['REQUEST_METHOD'] !== 'DELETE'){ return []; }
+
+        $value = self::PUT($key);
+        if(!is_numeric($value) || $value < 1){ return $default; }
+        return (int) $value;
+    }
+
+
+
+
 
     private static function formValueTypeCorrection($value){
         if(is_numeric($value)){
@@ -72,12 +122,28 @@ class RD //REQUEST DATA
         return self::formValueTypeCorrection($delegate);;
     }
 
-    private static function phpInputToValues(){
+
+    private static function phpInputToValues():array{
 
         $nestedFormData = [];
         $formData = [];
         $lines = file('php://input');
 
+
+        // JSON DATA TYPE :
+        if(isJson($lines[0])){
+            return json_decode($lines[0], true);
+        }
+
+
+        // X-WWW-FORM-URLENCODED DATA TYPE :
+        if(getHeader('Content-Type') === 'application/x-www-form-urlencoded'){
+            parse_str($lines[0], $formData);
+            return $formData;
+        }
+
+
+        // FORM-DATA DATA TYPE :
         //RAW KEY VALUE CREATE
         foreach($lines as $i =>  $line){
             $searchFieldLine = 'Content-Disposition: form-data; name="'; //38 characters
@@ -109,7 +175,6 @@ class RD //REQUEST DATA
         }
 
         return $nestedFormData;
-
     }
 
 }
